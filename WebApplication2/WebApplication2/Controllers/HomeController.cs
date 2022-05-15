d646a8f796f4399f4944c87c67251f8cde426e8e
LenDoc <lena.kirilenko13@gmail.com>
2022-05-15 18:26:24 +0300

    add HW Unit-07 WEB

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    int firstVal = 10;
+        //    int secondVal = 2;
+        //    int result = firstVal / secondVal;
+
+        //    ViewBag.Message = "Отладка приложения ASP.NET MVC!";
+
+        //    return View(result);
+        //}
+        public interface IEmailSender
+        {
+            void SendEmail();
+        }
+        public class MyEmailSender : IEmailSender
+        {
+            public void SendEmail() { }
+        }
+
+        public class PasswordResetHelper
+        {
+            public void ResetPassword()
+            {
+                IEmailSender mySender = new MyEmailSender();
+                // ...вызов методов интерфейса для конфигурировании настроек электронной почты... 
+                mySender.SendEmail();
+            }
+        }
+        [HttpGet]
+        public ViewResult AdminController()
+        {
+            return View();
+        }
+
+    }
+    //IEmailSender sender = new MyEmailSender();
+    //helper = new PasswordResetHelper(sender);
+
+
+}
